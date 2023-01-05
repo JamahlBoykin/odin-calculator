c15f2a39e4d07bb530fc18268265084e481b5cb9
JamahlBoykin <118788306+JamahlBoykin@users.noreply.github.com>
2023-01-04 18:01:54 -0800

    Add keyboard support

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ const operatorButtons = document.querySelectorAll('.operator');
 const clearButton = document.querySelector('.button-clear');
 const equalsButton = document.querySelector('.button-equals');
 const backspaceButton = document.querySelector('.button-backspace');
+const allButtons = document.querySelectorAll('.calculator > button');
+
+numberButtons.forEach((button) => {
+  button.setAttribute("tabIndex", -1);
+  button.setAttribute("onclick", "blur()");
+  button.setAttribute("onmouseleave", "blur()");
+});
 
 numberButtons.forEach((number) => {
   number.addEventListener('click', displayInput);
@@ -24,8 +31,66 @@ operatorButtons.forEach((operator) => {
 clearButton.addEventListener('click', reset);
 equalsButton.addEventListener('click', calcAnswer);
 backspaceButton.addEventListener('click', backspace);
+window.addEventListener('keydown', processKeyboardInput);
 
 
+
+function processKeyboardInput(e) {
+  switch (e.key) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case ".":
+      for (const number of numberButtons.values()) {
+        if (e.key === number.textContent) {
+          pressKey(number);
+          return;
+        }
+      }
+      break;
+    case "/":
+    case "*":
+    case "-":
+    case "+":
+      let key = e.key;
+      if (e.key === "/") {
+        key = "÷";
+      } else if (e.key === "*") {
+        key = "×";
+      }
+      for (const operator of operatorButtons.values()) {
+        if (key === operator.textContent) {
+          operator.click();
+        }
+      }
+      break;
+    case "Escape":
+      pressKey(clearButton);
+      break;
+    case "Enter":
+      pressKey(equalsButton);
+      break;
+    case "Delete":
+    case "Backspace":
+      pressKey(backspaceButton);
+      break;
+  }
+}
+
+function pressKey(key) {
+  key.click();
+  key.classList.add('active');
+  setTimeout(() => {
+    key.classList.remove('active');
+  }, 100);
+}
 
 function displayInput() {
   if (needsReset()) reset();
@@ -79,7 +144,7 @@ function calcAnswer() {
   }
   if (checkForEquation()) {
     secondNum = inputScreen.textContent;
-  } else if (repeatNum !== null && repeatOperator !== null) {
+  } else if (canRepeatEquals()) {
       secondNum = repeatNum;
       selectedOperator = repeatOperator;
   } else {
@@ -89,6 +154,7 @@ function calcAnswer() {
     inputScreen.textContent = "Can't divide by 0.";
     return;
   }
+  removeOperatorHighlighting();
   calcScreen.textContent = firstNum + " " + selectedOperator + " " + secondNum + " =";
   inputScreen.textContent = operate(selectedOperator, Number(firstNum), Number(secondNum));
   firstNum = inputScreen.textContent;
@@ -96,10 +162,15 @@ function calcAnswer() {
   repeatOperator = selectedOperator;
   secondNum = null;
   selectedOperator = null;
+
 }
 
 function checkForEquation() {
   return (firstNum !== null && selectedOperator !== null && !waitingForNum);
+}
+
+function canRepeatEquals() {
+  return (repeatNum !== null && repeatOperator !== null);
 }
 
 function backspace() {
